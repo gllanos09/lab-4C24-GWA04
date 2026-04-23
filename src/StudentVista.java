6402fa1f21e917d31219d5952913507be042ae60
haroldsantivanez-ship-it <harold.santivanez@tecsup.edu.pe>
2026-04-23 06:12:46 -0500

    cambios del nombre y adicion de funcion printStudentDeails

---
+++ src/StudentVista.java
@@ -0,0 +1,12 @@
+//Alumno: Harold Eduardo Santivañez García
+//Sección: C24A
+//Curso: Construcción y Prueba de Software
+
+public class StudentVista {
+    public void printStudentDetails(String nombre, double notas, String estado) {
+        System.out.println("--- REPORTE DEL ALUMNO ---");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Nota: " + notas);
+        System.out.println("Estado: " + estado );}
+}
+
